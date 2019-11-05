0a13fad395325059d0b8b6b149d668edcaebd9fc
Flash <188813950@qq.com>
2019-11-06 01:44:16 +0800

    分离输入模块

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
 import "../App.css";
+import TodoInput from './TodoInput';
 
 class Main extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      newTodo: '',
+      newTodo: '2222',
       todoList: [
-        { id: 1, title: '第一个待办' }
+        { id: 1, title: '第一个待办' },
+        { id: 2, title: '第二个待办' }
       ]
     }
   }
   render() {
     return (
       <div className="main">
-        <h1>我的待办</h1>
-        <div className="inputWrapper">
-          <input type="text" value={this.state.newTodo} />
+        <div><h1>我的待办</h1>
+          <div className="inputWrapper">
+          <TodoInput content={this.state.newTodo}/>
+          </div>
+          <ol>
+            {(this.state.todoList).map((item, index) => {
+              return <li>{item.title}</li>
+            })}
+          </ol>
         </div>
-      <ol>
-        {(this.state.todoList).map((item, index) => {
-          return <li>{item.title}</li>
-        })}
-      </ol>
       </div >
     )
   }
