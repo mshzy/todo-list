5a9effdec85383f19ab0886195a85c8357ce9097
Flash <188813950@qq.com>
2019-11-06 02:06:06 +0800

    再次更改

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "../App.css";
 import TodoInput from './TodoInput';
+import TodoItem from './TodoItem'
 
 class Main extends React.Component {
   constructor(props) {
@@ -14,16 +15,21 @@ class Main extends React.Component {
     }
   }
   render() {
+    let todos = this.state.todoList.map((item, index) => {
+      return (
+        <li>
+          <TodoItem todo={item} />
+        </li>
+      )
+    })
     return (
       <div className="main">
         <div><h1>我的待办</h1>
           <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo}/>
+            <TodoInput content={this.state.newTodo} />
           </div>
           <ol>
-            {(this.state.todoList).map((item, index) => {
-              return <li>{item.title}</li>
-            })}
+            {todos}
           </ol>
         </div>
       </div >
