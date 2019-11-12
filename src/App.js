27a8cf1b6ab5d6340b638f70ab74abfe16ace893
Flash <188813950@qq.com>
2019-11-13 02:20:18 +0800

    修改文件结构

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
+import TodoInput from './component/TodoInput';
+import TodoItem from './component/TodoItem';
 import './style/App.scss';
-import Main from "./main/Main";
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newTodo: '',
+      todoList: [
+
+      ]
+    }
+  }
+  addTodo() {
+    console.log('我得添加一个 todo 了')
+  }
   render() {
+    let todos = this.state.todoList.map((item, index) => {
+      return (
+        <li key={index} >
+          <TodoItem todo={item} />
+        </li>
+      )
+    })
+    console.log(todos)
     return (
-      <div>
-        <Main />
-      </div>
+      <div className="main">
+        <div><h1>我的待办</h1>
+          <div className="inputWrapper">
+            <TodoInput content={this.state.newTodo} addTodo={this.addTodo} />
+          </div>
+          <ol>
+            {todos}
+          </ol>
+        </div>
+      </div >
     )
   }
 }
-
 export default App;
